942dab66a63d1dc6a4b33513ddf69646f6851107
Yasmin Hannah <yasminhannah1510@gmail.com>
2023-07-04 10:29:00 +0800

    update future prediction

--- finalapplication.py
+++ finalapplication.py
@@ -253,7 +253,7 @@ def main():
         
         # Download the currency data
         start_date = pd.Timestamp('2013-01-01')
-        end_date = pd.Timestamp.today()  # Updated to current date
+        end_date = pd.Timestamp.today() 
         forex_data = yf.download('EURUSD=X', start=start_date, end=end_date)
         
         # Prepare the data for training
@@ -266,46 +266,51 @@ def main():
             seq_length = 30
         else:
             seq_length = 365
-        
+
         step = 1  # Step size for creating sequences
-        
+
         X = []
         y = []
-        
+
         for i in range(0, len(forex_close) - seq_length, step):
-            X.append(forex_close[i:i+seq_length])
-            y.append(forex_close[i+seq_length])
-        
+            X.append(forex_close[i:i + seq_length])
+            y.append(forex_close[i + seq_length])
+
         X = np.array(X)
         y = np.array(y)
-        
+
         # Split the data into training and test sets
         X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=123)
-        
+
         # Create and train the XGBoost model
         model = xgb.XGBRegressor(objective='reg:squarederror', n_estimators=1000, learning_rate=0.1, random_state=123)
         model.fit(X_train, y_train)
-        
+
         # Make predictions for the next 'num_days' days
         future_predictions = []
         X_pred = forex_close[-seq_length:].values
-        
+
         for _ in range(num_days):
             prediction = model.predict(X_pred.reshape(1, seq_length))
             future_predictions.append(prediction[0])
             X_pred = np.append(X_pred[1:], prediction)
-        
+
         # Create the timeline for the future predictions (using business days)
         last_date = forex_close.index[-1]
-        future_dates = pd.bdate_range(start=last_date + pd.DateOffset(days=1), periods=num_days)
+        next_date = last_date + pd.DateOffset(days=1)
+        end_date = next_date + pd.DateOffset(days=num_days)
+        future_dates = pd.bdate_range(start=next_date, end=end_date - pd.DateOffset(days=1))
 
         # Plot the predicted values
         fig = go.Figure()
         fig.add_trace(go.Scatter(x=forex_close.index, y=forex_close, mode='lines', name='Actual', visible='legendonly'))
         fig.add_trace(go.Scatter(x=future_dates, y=future_predictions, mode='lines', name='Predicted'))
 
-        fig.update_layout(title='EUR/USD Value Prediction for the Next {} Days'.format(num_days),
-                        xaxis_title='Date', yaxis_title='Currency Value')
+        fig.update_layout(
+            title='EUR/USD Value Prediction for the Next {} Days'.format(num_days),
+            xaxis_title='Date',
+            yaxis_title='Currency Value'
+        )
 
         st.plotly_chart(fig)
 
